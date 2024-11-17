b7557d5280f7363fffce045a2c9e288e7297480c
Vasilek91 <Hornstr@seznam.cz>
2024-11-17 20:24:18 +0100

    Update Otázka 2.sql

--- Otázka 2.sql
+++ Otázka 2.sql
@@ -2,22 +2,26 @@
 # ceny potravin od roku 2006 do roku 2018 | mzdy jsou od roku 2000 do 2021
 # 4 čísla, kolik se dá koupit litrů mléka v roce 2006 za průměrnou mzdu v roce 2006 a v roce 2018 a to samé pro chleba
 
-select
-potr.potraviny_rok,
-potr.potraviny_hodnota,
-potr.potraviny_jmeno,
-mzdy.potraviny_hodnota,
-round((mzdy.potraviny_hodnota/potr.potraviny_hodnota),2) as kupni_sila
-from t_petr_novotny_project_sql_primary_final as potr
-left join t_petr_novotny_project_sql_primary_final as mzdy on mzdy.mzdy_rok = potr.potraviny_rok 
-where (potr.potraviny_jmeno in ('mléko polotučné pasterované', 'Chléb konzumní kmínový'))
-and (potr.potraviny_rok = (
-        SELECT MIN(potraviny_rok) 
-        FROM t_petr_novotny_project_sql_primary_final 
-        )
-        or
-        potr.potraviny_rok = (
-        select max(potraviny_rok)
-        from t_petr_novotny_project_sql_primary_final
+SELECT
+    potr.rok_potraviny AS rok,
+    potr.hodnota_potravin AS cena_potraviny,
+    potr.nazev_potraviny AS potravina,
+    mzdy.prumerna_mzda AS prumerna_mzda,
+    ROUND((mzdy.prumerna_mzda / potr.hodnota_potravin), 2) AS kupni_sila
+FROM 
+    t_petr_novotny_project_sql_primary_final AS potr
+LEFT JOIN 
+    t_petr_novotny_project_sql_primary_final AS mzdy 
+    ON mzdy.rok_mzdy = potr.rok_potraviny
+WHERE 
+    potr.nazev_potraviny IN ('mléko polotučné pasterované', 'Chléb konzumní kmínový')
+    AND (potr.rok_potraviny = (
+            SELECT MIN(rok_potraviny) 
+            FROM t_petr_novotny_project_sql_primary_final 
+        ) 
+        OR 
+        potr.rok_potraviny = (
+            SELECT MAX(rok_potraviny)
+            FROM t_petr_novotny_project_sql_primary_final
         ))
-and mzdy.mzdy_industry_jmeno like 'republikový průměr';
+    AND mzdy.odvetvi_mzdy LIKE 'republikový průměr';
