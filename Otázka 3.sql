1f52b661f488ff79621862f0cbeb9c2215e1255b
Vasilek91 <Hornstr@seznam.cz>
2024-11-17 20:26:54 +0100

    Update Otázka 3.sql

--- Otázka 3.sql
+++ Otázka 3.sql
@@ -1,27 +1,27 @@
 # 3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
-with lonske_ceny as (
-select 
-	potraviny_hodnota, 
-	potraviny_jmeno,
-	potraviny_rok+1 as potraviny_rok
-from t_petr_novotny_project_sql_primary_final
-where 
-	hodnoty_typ like 'potraviny'
+WITH lonske_ceny AS (
+    SELECT 
+        hodnota_potravin AS lonska_cena, 
+        nazev_potraviny AS potravina,
+        rok_potraviny + 1 AS rok
+    FROM 
+        t_petr_novotny_project_sql_primary_final
+    WHERE 
+        typ_hodnoty LIKE 'potraviny'
 )
-select
-	#p.potraviny_hodnota, 
-	p.potraviny_jmeno,
-	#p.potraviny_rok, 
-	p2.potraviny_hodnota as lonske_ceny,
-	AVG(ROUND(((p.potraviny_hodnota - p2.potraviny_hodnota) / p2.potraviny_hodnota) * 100, 2)) AS prumerna_zmena_ceny
-from t_petr_novotny_project_sql_primary_final as p
-left join lonske_ceny as p2 on p2.potraviny_rok = p.potraviny_rok and p2.potraviny_jmeno = p.potraviny_jmeno 
-where 
-	p.hodnoty_typ like 'potraviny'
-	and p2.potraviny_hodnota is not null
-group by 
-	p.potraviny_jmeno 
-order by 
-	prumerna_zmena_ceny asc
-#limit 1;
+SELECT
+    p.nazev_potraviny AS potravina,
+    AVG(ROUND(((p.hodnota_potravin - p2.lonska_cena) / p2.lonska_cena) * 100, 2)) AS prumerna_zmena_ceny
+FROM 
+    t_petr_novotny_project_sql_primary_final AS p
+LEFT JOIN 
+    lonske_ceny AS p2 
+    ON p2.rok = p.rok_potraviny AND p2.potravina = p.nazev_potraviny
+WHERE 
+    p.typ_hodnoty LIKE 'potraviny'
+    AND p2.lonska_cena IS NOT NULL
+GROUP BY 
+    p.nazev_potraviny
+ORDER BY 
+    prumerna_zmena_ceny ASC;
