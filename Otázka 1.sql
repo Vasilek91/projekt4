ffb3da366cadacdff8fa1a63bc615571ca293779
Vasilek91 <Hornstr@seznam.cz>
2024-11-17 20:22:01 +0100

    Update Otázka 1.sql

--- Otázka 1.sql
+++ Otázka 1.sql
@@ -1,13 +1,14 @@
 # 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
-
+   
 SELECT 
-	mzdy_industry_name AS odvetvi,    
-	mzdy_year AS rok, 
-    mzdy_value AS 'Průměrná výplata'
+    odvetvi_mzdy AS odvetvi,    
+    rok_mzdy AS rok, 
+    prumerna_mzda AS 'Průměrná výplata',
+    prumerna_mzda - LAG(prumerna_mzda) OVER (PARTITION BY odvetvi_mzdy ORDER BY rok_mzdy) AS 'Meziroční změna'
 FROM 
-t_petr_novotny_project_sql_primary_final
+    t_petr_novotny_project_sql_primary_final
 WHERE 
-    mzdy_industry_name is not null
+    odvetvi_mzdy IS NOT NULL
 ORDER BY 
-	odvetvi,
-	rok;
+    odvetvi,
+    rok;
