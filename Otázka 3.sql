08ee4d1cbe2348ba954c57bbb5e6b7d47aba602d
Vasilek91 <Hornstr@seznam.cz>
2024-11-17 19:51:44 +0100

    Update Otázka 3.sql

--- Otázka 3.sql
+++ Otázka 3.sql
@@ -2,26 +2,26 @@
 
 with lonske_ceny as (
 select 
-	potraviny_value, 
-	potraviny_name,
-	potraviny_year+1 as potraviny_year
+	potraviny_hodnota, 
+	potraviny_jmeno,
+	potraviny_rok+1 as potraviny_rok
 from t_petr_novotny_project_sql_primary_final
 where 
-	values_type like 'potraviny'
+	hodnoty_typ like 'potraviny'
 )
 select
-	#p.potraviny_value, 
-	p.potraviny_name,
-	#p.potraviny_year, 
-	p2.potraviny_value as lonske_ceny,
-	AVG(ROUND(((p.potraviny_value - p2.potraviny_value) / p2.potraviny_value) * 100, 2)) AS prumerna_zmena_ceny
+	#p.potraviny_hodnota, 
+	p.potraviny_jmeno,
+	#p.potraviny_rok, 
+	p2.potraviny_hodnota as lonske_ceny,
+	AVG(ROUND(((p.potraviny_hodnota - p2.potraviny_hodnota) / p2.potraviny_hodnota) * 100, 2)) AS prumerna_zmena_ceny
 from t_petr_novotny_project_sql_primary_final as p
-left join lonske_ceny as p2 on p2.potraviny_year = p.potraviny_year and p2.potraviny_name = p.potraviny_name 
+left join lonske_ceny as p2 on p2.potraviny_rok = p.potraviny_rok and p2.potraviny_jmeno = p.potraviny_jmeno 
 where 
-	p.values_type like 'potraviny'
-	and p2.potraviny_value is not null
+	p.hodnoty_typ like 'potraviny'
+	and p2.potraviny_hodnota is not null
 group by 
-	p.potraviny_name 
+	p.potraviny_jmeno 
 order by 
 	prumerna_zmena_ceny asc
 #limit 1;
