95165fdf58a86cce0bffa609e37f7a8dc0d58e70
Vasilek91 <Hornstr@seznam.cz>
2024-12-22 13:40:22 +0100

    Update t_petr_Novotny_project_sql_secondary_final.sql

--- t_petr_Novotny_project_sql_secondary_final.sql
+++ t_petr_Novotny_project_sql_secondary_final.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE TABLE t_petr_novotny_project_sql_secondary_final AS
+CREATE TABLE t_petr_novotny_project_sql_secondary_final AS
 SELECT
-    e.country AS nazev_zeme, 
-    e.year AS rok, 
-    e.GDP AS hdp
+    e.country AS country_name, 
+    e.year AS year, 
+    e.GDP AS gdp
 FROM 
     economies e
 LEFT JOIN 
