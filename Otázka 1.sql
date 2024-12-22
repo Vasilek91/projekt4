9bfc97c4111fbfc752936c385ba7da274e92ec61
Vasilek91 <Hornstr@seznam.cz>
2024-12-22 14:43:30 +0100

    Update Otázka 1.sql

--- Otázka 1.sql
+++ Otázka 1.sql
@@ -3,8 +3,8 @@
 SELECT 
     odvetvi_mzdy AS odvetvi,    
     rok_mzdy AS rok, 
-    prumerna_mzda AS 'Průměrná výplata',
-    prumerna_mzda - LAG(prumerna_mzda) OVER (PARTITION BY odvetvi_mzdy ORDER BY rok_mzdy) AS 'Meziroční změna'
+    prumerna_mzda AS 'prumerna_vyplata',
+    prumerna_mzda - LAG(prumerna_mzda) OVER (PARTITION BY odvetvi_mzdy ORDER BY rok_mzdy) AS 'mezirocni_zmena'
 FROM 
     t_petr_novotny_project_sql_primary_final
 WHERE 
